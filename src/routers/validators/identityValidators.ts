77c01f039284852ee3b558ce6e63e50ead57875c
maxwellpothier <pothiermaxwell@gmail.com>
2023-03-07 20:36:17 -0700

    adds the validators for create

--- src/routers/validators/identityValidators.ts
+++ src/routers/validators/identityValidators.ts
@@ -2,7 +2,9 @@ import { body } from "express-validator";
 import { handleInputErrors } from "../../utils/middlewareUtils";
 
 export const createUserValidators = [
-	body("username").trim().isString(),
+	body("username").trim().not().isEmpty().withMessage("Username cannot be empty"),
 	body("password").trim().isLength({min: 8}).withMessage("Password must be 8 characters long"),
+	body("firstName").trim().not().isEmpty().withMessage("First name cannot be empty"),
+	body("lastName").trim().not().isEmpty().withMessage("Last name cannot be empty"),
 	handleInputErrors,
 ];
