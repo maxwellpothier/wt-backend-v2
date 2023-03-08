16f4b190516caa96e2eb2a6b76bbf8d66bb83815
maxwellpothier <pothiermaxwell@gmail.com>
2023-03-07 21:00:34 -0700

    handle confirmPassword being sent

--- src/routers/validators/identityValidators.ts
+++ src/routers/validators/identityValidators.ts
@@ -4,6 +4,11 @@ import { handleInputErrors } from "../../utils/middlewareUtils";
 export const createUserValidators = [
 	body("username").trim().not().isEmpty().withMessage("Username cannot be empty"),
 	body("password").trim().isLength({min: 8}).withMessage("Password must be 8 characters long"),
+	body("confirmPassword").custom((value, {req}) => {
+		if (value !== req.body.password) {
+			throw new Error("Passwords do not match");
+		}
+	}),
 	body("firstName").trim().not().isEmpty().withMessage("First name cannot be empty"),
 	body("lastName").trim().not().isEmpty().withMessage("Last name cannot be empty"),
 	handleInputErrors,
